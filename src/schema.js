b2f7701161144778a0cfe0495b7f70c06014026d
unknown <alpha.keita@efrei.net>
2023-04-26 18:22:10 +0200

    update schema

--- src/schema.js
+++ src/schema.js
@@ -2,91 +2,111 @@ import { buildSchema } from 'graphql';
 
 export const schema = buildSchema(`
 
-type Course {
-    id_courses: Int!
-    module: String
-    ue: String
-    type: String
-    coef: Float
-    grades: [Grade]
-    teachers: [Teacher]
-  }
-  
-  type Grade {
-    id_grades: Int!
-    note: Float
-    course: Course
-    student: Student
-    teacher: Teacher
-  }
-  
-  type Student {
-    id_student: Int!
-    num_student: String
-    grades: [Grade]
-    user: User
-  }
-  
-  type Teacher {
-    id_teacher: Int!
-    course: Course
-    user: User
-    grades: [Grade]
-  }
-  
-  type User {
-    id_user: Int!
-    username: String
-    password: String
-    email: String
-    first_name: String
-    last_name: String
-    gender: String
-    date_of_birth: String
-    student: [Student]
-    teacher: [Teacher]
-  }
+type Courses {
+  id_courses: Int!
+  module: String!
+  ue: String!
+  coef: Float!
+  courses_teacher: [Courses_teacher!]!
+  grades: [Grades!]!
+}
 
-  input UserInput {
-    username: String
-    password: String
-    email: String
-    first_name: String
-    last_name: String
-    gender: String
-    date_of_birth: String
-  }
+type Grades {
+  id_grades: Int!
+  note: Float!
+  id_courses: Int!
+  id_student: Int!
+  id_teacher: Int!
+  courses: Courses!
+  student: Student!
+  teacher: Teacher!
+}
 
-  input StudentInput {
-    num_student: String
-    id_user: Int
-  }
+type Student {
+  id_student: Int!
+  num_student: String
+  id_user: Int!
+  grades: [Grades!]!
+  user: User!
+}
 
-  input CourseInput {
-    module: String!
-    ue: String
-    type: String
-    coef: Float
-  }
+type Teacher {
+  id_teacher: Int!
+  id_user: Int!
+  courses_teacher: [Courses_teacher!]!
+  grades: [Grades!]!
+  user: User!
+}
 
-    type Query {
-        getUsers : [User]
-        getStudents: [Student]
-        getCourses: [Course]
-    }
+type User {
+  id_user: Int!
+  username: String!
+  password: String!
+  email: String!
+  first_name: String!
+  last_name: String!
+  gender: String
+  date_of_birth: String!
+  student: [Student!]!
+  teacher: [Teacher!]!
+}
 
-    type Mutation {
-        createUser(value: UserInput ) : User
-        updateUser(id: Int, value: UserInput) : User
-        deleteUser(id: Int) : User
+type Courses_teacher {
+  id_courses: Int!
+  id_teacher: Int!
+  courses: Courses!
+  teacher: Teacher!
+}
 
-        createStudent(value: StudentInput ) : Student
-        updateStudent(id: Int, value: StudentInput) : Student
-        deleteStudent(id: Int) : Student
+input UserInput {
+  username: String
+  password: String
+  email: String
+  first_name: String
+  last_name: String
+  gender: String
+  date_of_birth: String
+}
 
-        createCourse(value: CourseInput ) : Course
-        updateCourse(id: Int, value: CourseInput) : Course
-        deleteCourse(id: Int) : Course
-    }
+input StudentInput {
+  num_student: String
+  id_user: Int
+}
 
+input CourseInput {
+  module: String!
+  ue: String
+  type: String
+  coef: Float
+}
+
+input TeacherInput {
+  id_courses: Int
+  id_user: Int
+}
+
+type Query {
+  getUsers : [User]
+  getStudents: [Student]
+  getCourses: [Course]
+  getTeachers: [Teacher]
+}
+
+type Mutation {
+  createUser(value: UserInput ) : User
+  updateUser(id: Int, value: UserInput) : User
+  deleteUser(id: Int) : User
+
+  createStudent(value: StudentInput ) : Student
+  updateStudent(id: Int, value: StudentInput) : Student
+  deleteStudent(id: Int) : Student
+
+  createCourse(value: CourseInput ) : Course
+  updateCourse(id: Int, value: CourseInput) : Course
+  deleteCourse(id: Int) : Course
+
+  createTeacher(value: TeacherInput ) : Teacher
+  updateTeacher(id: Int, value: TeacherInput) : Teacher
+  deleteTeacher(id: Int) : Teacher
+}
 `)
