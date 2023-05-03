5424b564a446d01cbe7484cb470bf1b8be2eda8d
keita_al <alpha.keita@lcl.fr>
2023-05-03 13:58:04 +0200

    added logic to student controller

--- src/controlleurs/student.controller.js
+++ src/controlleurs/student.controller.js
@@ -2,7 +2,22 @@ import studentModel from '../models/student.model.js';
 
 export default {
     getStudents: () => studentModel.getStudents(),
-    createStudent: (value) => studentModel.createStudent(value),
-    updateStudent: (id, value) => studentModel.updateStudent(id, value),
-    deleteStudent: (id) => studentModel.deleteStudent(id)
+    createStudent: ({value}) => {
+        const { num_student } = value
+        const regexMatch = /^[A-Z]\d{5}$/;
+        const isValid = regexMatch.test(num_student)
+
+        if(!isValid) throw new Error("Le format du/des données saisi(s) sont incorrect")
+        
+        studentModel.createStudent(value)
+    },
+    updateStudent: ({id, value}) => {
+        const { num_student } = value
+        const regexMatch = /^[A-Z]\d{5}$/;
+        const isValid = regexMatch.test(num_student)
+
+        if(!isValid) throw new Error("Le format du/des données saisi(s) sont incorrect")
+        studentModel.updateStudent(id, value)
+    },
+    deleteStudent: ({id}) => studentModel.deleteStudent(id)
 }
